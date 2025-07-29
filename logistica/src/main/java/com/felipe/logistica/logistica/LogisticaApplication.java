1411c8c44fc63511d1f22cdadc5a42fafa24d876
Aslotbr <felipesiqueira2209@gmail.com>
2025-07-29 18:03:26 -0300

    SpringBoot

---
+++ logistica/src/main/java/com/felipe/logistica/logistica/LogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.logistica.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaApplication.class, args);
+	}
+
+}
